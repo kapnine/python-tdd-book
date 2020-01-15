6bfbd32ca2328823c5eb09dd12c932c12328bd35
Justin Kapacinskas <justin.kapacinskas@imtins.com>
2020-01-15 10:58:35 -0600

    try changing messages implementation to show mocky test fails

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 
-# Create your views here.
+def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists',
+        'body text tbc',
+        'noreply@superlists',
+        [email]
+    )
+    messages.add_message(
+        request,
+        messages.SUCCESS,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
